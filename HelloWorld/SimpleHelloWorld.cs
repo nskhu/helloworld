ec865eacb0b53c419a66014b427a8da076658b7a
nskhu12 <nskhu12@gmail.com>
2023-11-21 14:58:59 +0400

    Add project files.

---
+++ HelloWorld/SimpleHelloWorld.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    
+    public static class SimpleHelloWorld
+    {
+        
+        public static string ReturnHelloWorld()
+        {
+            // TODO: Return string "Hello, world!";
+            
+            var greeting = "Hello, world!";
+            System.Console.WriteLine(greeting);
+            return greeting;
+        }
+    }
+}
